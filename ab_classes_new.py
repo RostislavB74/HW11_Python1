d871f49233c55a2d83212222d37e2867d2cabce7
Rostyslav Bysko <132708200+RostislavB74@users.noreply.github.com>
2023-07-16 16:01:45 +0300

    ok

---
+++ ab_classes_new.py
@@ -0,0 +1,78 @@
+from collections import UserDict
+
+
+class Field:
+
+    def __init__(self, value) -> None:
+        self.value = value
+
+    def __str__(self) -> str:
+        return self.value
+
+    def __repr__(self) -> str:
+        return str(self)
+
+
+class Name(Field):
+    ...
+
+
+class Phone(Field):
+    ...
+
+
+class Record:
+
+    def __init__(self, name: Name, phone: Phone = None) -> None:
+        self.name = name
+        self.phones = []
+        if phone:
+            if isinstance(phone, list):
+                self.phones.extend(phone)
+            else:
+                self.phones.append(phone)
+
+    def add_phone(self, phone: Phone):
+        if isinstance(phone, list):
+            for i in range(len(phone)):
+                if phone[i] not in [p for p in self.phones]:
+                    self.phones.extend(phone)
+
+            return f"The contact {self.name} was updated"
+        else:
+            if phone.value not in [p.value for p in self.phones]:
+                self.phones.append(phone)
+                return f"phone {phone} add to contact {self.name}"
+        return f"{phone} present in phones of contact {self.name}"
+
+    def change_phone(self, old_phone, new_phone):
+        for idx, p in enumerate(self.phones):
+            if old_phone.value == p.value:
+                self.phones[idx] = new_phone
+                return f"old phone {old_phone} change to {new_phone}"
+        return f"{old_phone} not present in phones of contact {self.name}"
+
+    def __str__(self) -> str:
+        return f"{self.name}: {', '.join(str(p) for p in self.phones)}"
+
+    def remove_phone(self, phone):
+        for idx, p in enumerate(self.phones):
+            if phone.value == p.value:
+                # print(self.phones)
+                old_phone = (self.phones[idx])
+                self.phones.remove(self.phones[idx])
+                return f"The phone {old_phone} is deleted"
+        return f"{phone} not present in phones of contact {self.name}"
+
+
+class AddressBook(UserDict):
+
+    def add_record(self, record: Record):
+        self.data[str(record.name)] = record
+        return f"Contact {record} add success"
+    # def add_record1(self, record: Record):
+    #     self.data[str(record.name)] = record
+    #     return f"Contact {record} add success"
+
+    def __str__(self) -> str:
+        return "\n".join(str(r) for r in self.data.values())
---
+++ main_sun_day.py
@@ -0,0 +1,141 @@
+from ab_classes_new import AddressBook, Name, Phone, Record
+
+address_book = AddressBook()
+
+
+def input_error(func):
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except IndexError as e:
+            print(
+                f"{e} Give me a name one phone number or several phone numbers please")
+        except TypeError as e:
+            print(
+                f"{e} Give me a name one phone number or several phone numbers please")
+        except UnboundLocalError:
+            print("Contact exists, you can add one number per step")
+        except ValueError as e:
+            print(
+                f"{e} Give me a name one phone number or several phone numbers please")
+        except AttributeError as e:
+            print(
+                f"{e} Give me a name one phone number or several phone numbers please")
+    return wrapper
+
+
+@input_error
+def add_contact(*args):
+    name = Name(args[0])
+    if len(args) == 2:
+        # name = Name(args[0])
+        phone = Phone(args[1])
+        rec: Record = address_book.get(str(name))
+        if rec:
+            return rec.add_phone(phone)
+        rec = Record(name, phone)
+    if len(args) > 2:
+        # name = Name(args[0])
+        list_phones = []
+        rec: Record = address_book.get(str(name))
+        if rec:
+            for i in range(1, len(args)):
+                list_phones.append(Phone(args[i]))
+            return rec.add_phone(list_phones)
+        else:
+            for i in range(1, len(args)):
+                list_phones.append(Phone(args[i]))
+            rec = Record(name, list_phones)
+
+            return address_book.add_record(rec)
+    else:
+        return "Unknown command"
+
+# змінити
+
+
+@input_error
+def change_phone(*args):
+    name = Name(args[0])
+    old_phone = Phone(args[1])
+    new_phone = Phone(args[2])
+    rec: Record = address_book.get(str(name))
+    # print(rec)
+    if rec:
+        return rec.change_phone(old_phone, new_phone)
+    return f"No contact {name} in address book"
+
+
+# Вийти
+def exit_command(*args):
+    return "Good bye!"
+
+
+# показати контакт
+@input_error
+def get_phone(*args):
+    name = Name(args[0])
+    # return f"User {name.value}"
+    return f"User {address_book.get(str(name))}"
+
+
+# Привіт
+def hello(*args):
+    return "How can I help you?"
+
+
+# Невідома команда пуста команда
+def no_command(*args, **kwargs):
+    return "Unknown command"
+
+
+# показати все
+def show_all_command(*args):
+    return address_book
+
+# Видалити
+
+
+@input_error
+def remove_phone(*args):
+    name = Name(args[0])
+    phone = Phone(args[1])
+    rec: Record = address_book.get(str(name))
+    # print(rec)
+    if rec:
+        return rec.remove_phone(phone)
+    return f"No contact {name} in address book"
+
+
+# Команди додати, змінити, видалити, вихід, показати все, показати контакт
+COMMANDS = {
+    add_contact: ("add ", "+ "),
+    change_phone: ("change ", "зміни "),
+    remove_phone: ("remove ", "delete ", "del ",),
+    exit_command: ("good bye", "bye", "exit", "end", "close", "quit"),
+    show_all_command: ("show all", "show"),
+    get_phone: ("phone ",)
+}
+
+
+def parser(text: str):
+    for cmd, kwds in COMMANDS.items():
+        for kwd in kwds:
+            if text.lower().startswith(kwd):
+                data = text[len(kwd):].strip().split()
+                return cmd, data
+    return no_command, []
+
+
+def main():
+    while True:
+        user_input = input(">>>")
+        cmd, data = parser(user_input)
+        result = cmd(*data)
+        print(result)
+        if cmd == exit_command:
+            break
+
+
+if __name__ == "__main__":
+    main()
